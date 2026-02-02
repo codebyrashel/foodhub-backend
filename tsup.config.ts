416c57fe44c8ef3817690b67e660b292360ace6e
codebyrashel <codebyrashel@gmail.com>
2026-02-02 23:52:14 +0600

    restored previous state for deployment

--- tsup.config.ts
+++ tsup.config.ts
@@ -7,10 +7,11 @@ export default defineConfig({
   target: "node20",
   outDir: "api",
   external: ["pg-native"],
-  skipNodeModulesBundle: true, // Prevents bundling node_modules (Avoids Vercel crashes)
-  shims: true, // Fixes __dirname and other ESM compatibility issues
-  outExtension() {
-    return { js: ".mjs" }; // Outputs server.mjs
+  skipNodeModulesBundle: true,
+  shims: true,
+  outExtension({ format }) {
+    if (format === "esm") return { js: ".mjs" };
+    return { js: ".js" };
   },
-  clean: true, // Clears the api/ folder before every build
+  clean: true,
 });
